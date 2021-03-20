96d9b1720c0517a8b201cd07c554d3e7ec185c51
Lars Brinkhoff <lars@nocrew.org>
2021-03-20 14:35:32 +0000

    Files extracted from 169281.tape

---
+++ jms/init.scm
@@ -0,0 +1,9 @@
+File 1)	DSK:INIT.BAK[21477,243135]	created: 0954 13-JUL-83
+File 2)	DSK:INIT.SAI[21477,243135]	created: 1209 14-JUL-88
+
+1)1	setdo("GAMES","PNETS","UPL","MPL","TYMNET","SPL");	! do list;
+1)	RPGini;					! set this all up;
+****
+2)1	setdo("PNETS","UPL","MPL","TYMNET","SPL");	! do list;
+2)	RPGini;					! set this all up;
+**************
