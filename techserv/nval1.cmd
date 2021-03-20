fc5b968859aa675fe92bc30d0c926bcc7d3fff5a
Lars Brinkhoff <lars@nocrew.org>
2021-03-20 22:58:58 +0000

    Files extracted from 169281.tape

---
+++ techserv/nval1.cmd
@@ -0,0 +1,530 @@
+;
+
+tymshare  0105  08-aug-1985
+
+-o all
+akwok
+
+-
+Did not set WC RC OP GD TD ST JL XC
+-run usrval
+
+
+
+
+
+User Validation System - ver 1.7
+
+
+
+*l u c amykwok enarg065net isistech sjhnshc lwblake rmc295 promobis vl0961
+Building Circuit to the CUD host.
+
+
+
+    Username :     AMYKWOK
+    Gan :          21477
+    Uun :          22054
+    Dist :         229
+    Passdate :     3-30-84
+    Lic :          Tym Non-bill
+
+    (1) Class :    2
+        Group :    1
+        Host :     22   - Home
+
+    (2) Class :    4
+        Host :     1897 - Home
+
+
+
+    Username :     ENARG065NET
+    Gan :          30157
+    Uun :          35410
+    Dist :         228
+    Passdate :     12-9-81
+    Lic :          Bill No-colon
+
+    (1) Class :    32
+        Host :     493  - Home
+
+
+
+    Username :     ISISTECH
+    Gan :          21476
+    Uun :          302460
+    Dist :         229
+    Passdate :     11-5-84
+    Lic :          Non-bill
+
+    (1) Class :    0
+        Group :    0
+        Host :     25   - Home
+
+
+
+    Username :     SJHNSHC
+    Gan :          25007
+    Uun :          225456
+    Dist :         211
+    Passdate :     10-7-83
+    Lic :          Bill No-colon
+
+    (1) Class :    4
+        Host :     2357 - Home
+
+    (2) Class :    4
+        Host :     4024 - Home
+
+
+
+    Username :     LWBLAKE
+    Gan :          25007
+    Uun :          24136
+    Dist :         211
+    Passdate :     8-1-84
+    Lic :          Bill No-colon
+
+    (1) Class :    4
+        Host :     2357 - Home
+
+    (2) Class :    4
+        Host :     4024 - Home
+
+
+
+    Username :     RMC295
+    Gan :          42617
+    Uun :          256143
+    Dist :         135
+    Passdate :     7-18-83
+    Lic :          Bill
+
+    (1) Class :    4
+        Host :     3920 - Home
+        Host :     3922 - Home
+        Host :     5850 - Home
+
+
+
+    Username :     PROMOBIS
+    Gan :          43120
+    Uun :          404606
+    Dist :         76
+    Passdate :     9-12-84
+    Lic :          Bill
+
+    (1) Class :    2
+        Group :    1
+        Host :     51   - Home
+
+7
+
+
+    Username :     VL09617
+    Gan :          44643
+    Uun :          410102
+    Dist :         116
+    Passdate :     10-1-84
+    Lic :          Bill
+
+    (1) Class :    2
+        Group :    1
+        Host :     43   - Home
+
+Username :aee aenip 1wui173ts x25tstvie mucsge0200 chi01495 lroa383wnrs
+
+Username not found in Cud.
+
+
+    Username :     1WUI173TS
+    Gan :          22374
+    Uun :          371275
+    Dist :         228
+    Passdate :     3-26-81
+    Lic :          Bill No-colon
+
+    (1) Class :    4
+        Host :     340  - Home
+        Host :     699  - Home
+
+
+
+    Username :     X25TSTVIE
+    Gan :          33117
+    Uun :          357266
+    Dist :         214
+    Passdate :     1-13-81
+    Lic :          Bill No-colon
+
+    (1) Class :    4
+        Group :    2
+        Host :     2332 - Home
+
+    (2) Class :    5
+        Host :     1864 - Home
+
+
+
+    Username :     MUCSGE0200
+    Gan :          25665
+    Uun :          176226
+    Dist :         136
+    Passdate :     4-16-83
+    Lic :          Bill No-colon
+
+    (1) Class :    24
+        Host :     345  - Home
+
+    (2) Class :    24
+        Host :     3551 - Home
+
+
+
+    Username :     CHI01495
+    Gan :          16302
+    Uun :          422757
+    Dist :         165
+    Passdate :     12-18-84
+    Lic :          Tym Non-bill
+
+    (1) Class :    2
+        Group :    1
+        Host :     85   - Home
+
+
+
+    Username :     LROA383WNRS
+    Gan :          30270
+    Uun :          36452
+    Dist :         228
+    Passdate :     12-16-81
+    Lic :          Bill No-colon
+
+    (1) Class :    1
+        Host :     61   - Home
+
+    (2) Class :    1
+        Host :     2454 - Home
+
+Username :aeaenip tfsc12 fhwa10rghst alb13 akwok
+
+Username not found in Cud.
+
+Username not found in Cud.
+
+
+    Username :     FHWA10RGHST
+    Gan :          36652
+    Uun :          327070
+    Dist :         225
+    Passdate :     9-2-83
+    Lic :          Bill No-colon
+
+    (1) Class :    4
+        Host :     1607 - Home
+        Host :     1606 - Home
+
+
+
+    Username :     ALB13
+    Gan :          22425
+    Uun :          214641
+    Dist :         131
+    Passdate :     6-15-84
+    Lic :          Bill No-colon
+
+    (1) Class :    4
+        Group :    2
+        Host :     327  - Home
+
+
+Username not found in Cud.
+Username :545 tomasir
+
+Cud circuit zapped
+*l u c 545 tomasir
+
+Building Circuit to the CUD host.
+
+
+Username not found in Cud.
+
+Username not found in Cud.
+Username :
+
+*
+*l u a 10 amykwok enarg065net isistech sjhnshc lwblake rmc295 promobis
+
+Building Circuit to the Val Accounting host.
+
+
+
+    Username :     AMYKWOK
+    Sys. Type :    Tymcom-10
+    Gan :          21477
+    Cost Code :    3457
+    Dist :         229
+    Cid :          13516
+    Cust Name :    TYMNET, INC.
+    Salesman :     0
+    Currency :     US Dollars
+    Language :     English
+    Time Zone :    Pacific  - Subject to daylight savings
+    Hosts :        22 25 28 33 37 70
+
+
+No acctg record for this uun/system type
+
+
+    Username :     ISISTECH
+    Sys. Type :    Tymcom-10
+    Gan :          21476
+    Cost Code :    5207
+    Dist :         229
+    Cid :          20490
+    Cust Name :    TYMNET, INC.
+    Salesman :     0
+    Currency :     US Dollars
+    Language :     English
+    Time Zone :    Pacific  - Subject to daylight savings
+    Hosts :        25 33
+
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+Username :vl09617 aeaenip 1wui173ts
+No acctg record for this uun/system type
+
+Username not found in Cud.
+
+No acctg record for this uun/system type
+
+Username :x25tstvie mucsge01 200 chi01495 lroa383wnrs tfsc12
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+Username not found in Cud.
+
+Username :fhwa10rghst alb13
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+Username :
+
+*l u a 940 amykwok enarg065net isisteh ch sjhnshc lwblake rmc295
+
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+Username :promobis vl09617 aeaenip 1wui173ts
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+Username not found in Cud.
+
+No acctg record for this uun/system type
+
+Username :x25tstvie
+No acctg record for this uun/system type
+
+Username :mucc   sge0200 chi01495 lroa383wnrs tfsc12 fhwa10rghst alb13
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+Username not found in Cud.
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+Username :
+
+*l u a 370 amykwok enarg065net isistech sjhnshc lwblake rmc295 promobis
+
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+
+    Username :     PROMOBIS
+    Sys. Type :    Tymcom-370
+    Gan :          43120
+    Cost Code :    5006
+    Dist :         76
+    Cid :          39366
+    Cust Name :    SODICAM
+    Salesman :     40
+    Currency :     French Francs
+    Language :     French
+    Time Zone :    Western Europe  - Subject to daylight savings
+    Hosts :        51
+
+
+Username :vl09617 aeaenip 1wui173ts
+
+    Username :     VL09617
+    Sys. Type :    Tymcom-370
+    Gan :          44643
+    Cost Code :    3130
+    Dist :         116
+    Cid :          38339
+    Cust Name :    FORD MOTOR COMPANY
+    Salesman :     1
+    Currency :     US Dollars
+    Language :     English
+    Time Zone :    Eastern  - Subject to daylight savings
+    Hosts :        43
+
+
+Username not found in Cud.
+
+No acctg record for this uun/system type
+
+Username :x25tstvie mucsge0200 chi01495 lroa383wnrs tfsc12 fhwa10rghst
+No acctg record for this uun/system type
+
+No acctg record for this uun/system type
+
+
+    Username :     CHI01495
+    Sys. Type :    Tymcom-370
+    Gan :          16302
+    Cost Code :    2059
+    Dist :         165
+    Cid :          35247
+    Cust Name :    MAD/CHI TAX
+    Salesman :     0
+    Currency :     US Dollars
+    Language :     English
+    Time Zone :    Central  - Subject to daylight savings
+    Hosts :        43
+
+
+No acctg record for this uun/system type
+
+Username not found in Cud.
+
+No acctg record for this uun/system type
+
+Username :alb13
+No acctg record for this uun/system type
+
+Username :
+
+*l u l 36 amykwok enarg065net isistech sjhnshc lwblake rmc295 promobis
+
+Building Circuit to Lud host : 36.
+
+
+Username AMYKWOK not found in Lud on host 36
+
+Username ENARG065NET not found in Lud on host 36
+
+Username ISISTECH not found in Lud on host 36
+
+Username SJHNSHC not found in Lud on host 36
+
+Username LWBLAKE not found in Lud on host 36
+
+Username RMC295 not found in Lud on host 36
+
+Username PROMOBIS not found in Lud on host 36
+
+Username :vl09617 aeaenip 1wui173ts x25tstvie mucsge0200
+Username VL09617 not found in Lud on host 36
+
+Username not found in Cud.
+Username AEAENIP not found in Lud on host 36
+
+Username 1WUI173TS not found in Lud on host 36
+
+Username X25TSTVIE not found in Lud on host 36
+
+Username MUCSGE0200 not found in Lud on host 36
+
+Username :chi01495 lroa383wnrs tfsc12 fhwa10rghst alb13
+Username CHI01495 not found in Lud on host 36
+
+Username LROA383WNRS not found in Lud on host 36
+
+Username not found in Cud.
+Username TFSC12 not found in Lud on host 36
+
+Username FHWA10RGHST not found in Lud on host 36
+
+Username ALB13 not found in Lud on host 36
+
+Username :ssvalsup techserv
+
+    Username :     SSVALSUP
+    Gan :          11447
+    Uun :          74305
+    Dist :         3
+    Time Zone :    Pacific  - Subject to daylight savings
+    Login Quota :  100000
+    Logout Quota : 100000
+    Tru Budget :   0
+    Init File :    (MIKEY)INIT
+    Lic :          Gfd Tym
+    Mode :         Xexec
+
+
+    Username :     TECHSERV
+    Gan :          21476
+    Uun :          261167
+    Dist :         201
+    Time Zone :    Pacific  - Subject to daylight savings
+    Login Quota :  5000
+    Logout Quota : 5000
+    Tru Budget :   5000
+    Init File :    (TECHSERV)INI
+    Lic :          Gfd Cont Tym
+    Mode :         Xexec
+
+Username :
+
+*Q
+
+EXIT
+
+-q
+?Q?
+
+-exit
+    
